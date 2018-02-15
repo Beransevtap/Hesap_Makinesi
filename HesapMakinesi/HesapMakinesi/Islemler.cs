3cc3fe5798d2be61edb009924630af89e4dc35a0
sevtapberan <36489056+Beransevtap@users.noreply.github.com>
2018-02-15 23:06:23 +0300

    Add files via upload

---
+++ HesapMakinesi/HesapMakinesi/Islemler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    class Islemler
+    {
+        float sonuc = 0;
+        public float Toplama(float a, float b)
+        {
+            return sonuc=(a + b);
+        }
+         
+        public float Cikarma(float a, float b)
+        {
+            return sonuc=(a - b);
+        }
+
+    }
+}
---
+++ HesapMakinesi/HesapMakinesi/Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    class Test
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Islemler islem = new Islemler();
+            float sonuc = 0;
+            Console.WriteLine("*************HESAP MAKİNESİ*************");
+            Console.WriteLine("Lütfen işlem yapılacak ilk sayıyı giriniz...");
+            float deger1=float.Parse(Console.ReadLine());
+            Console.WriteLine("Lütfen işlem yapılacak ikinci sayıyı giriniz...");
+            float deger2 = float.Parse(Console.ReadLine());
+            Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine("***********************İŞLEMLER***********************");
+            Console.WriteLine("Toplama işlemi için 1 seçiniz...");
+            Console.WriteLine("Çıkarma işlemi için 2 seçiniz...");
+            Console.WriteLine("Seçiminizi Giriniz...");
+            int secim=int.Parse(Console.ReadLine());
+            if (secim==1)
+            {
+               Console.WriteLine(islem.Toplama(deger1, deger2));
+
+            }
+            else if(secim==2)
+            {
+                Console.WriteLine(islem.Cikarma(deger1, deger2));  
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}

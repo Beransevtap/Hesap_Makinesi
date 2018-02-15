4f56e1d960a0ec92ce7d56262b6d4587e84d5eb3
Elif FINDIK <elifindik9@gmail.com>
2018-02-15 22:38:14 +0200

    Add files via upload

---
+++ Islemler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HesapMakinesi
+{
+    class Islemler
+    {
+        float deger1;
+        float deger2;
+        float sonuc = 0;
+
+        public float Carpma(float deger1, float deger2)
+        {
+            sonuc = deger1 * deger2;
+            return sonuc;
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            float sonuc=0,deger1,deger2;
+            Islemler obj = new Islemler();
+            Console.WriteLine("Çarpma işlemini yapacağınız sayıları giriniz:...");
+            deger1 = Convert.ToInt16(Console.ReadLine());
+            deger2 = Convert.ToInt16(Console.ReadLine());
+            sonuc = obj.Carpma(deger1,deger2);
+
+            Console.WriteLine(sonuc);
+            Console.Read();
+        }
+    }
+}
